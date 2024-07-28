5265cd595f96be041a0ad4231b0b5766f26d61cc
bng16 <biswajitnag161619@gmail.com>
2024-07-28 20:05:48 +0530

    2nd done

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-let n=5;
-let result=1;
+// let n=5;
+// let result=1;
 
-function factorial(n,result){
-    if(n===1){
-        result *=n;
-        return result;
+// function factorial(n,result){
+//     if(n===1){
+//         result *=n;
+//         return result;
+//     }
+//     result*=n;
+//     return factorial(n-1,result);
+// }
+
+// console.log(factorial(5,result));
+
+
+
+let n1=0;
+let n2=1;
+let nth=6;
+
+function fibonacchi(n1,n2,nth){
+    if (nth>0){
+        console.log(n1);
+        return fibonacchi(n2,n1+n2,nth-1);
     }
-    result*=n;
-    return factorial(n-1,result);
+    
+
 }
 
-console.log(factorial(5,result));
+fibonacchi(n1,n2,nth);
